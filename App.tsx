23c53d20a0efb8541d22bf81a1a9a594db76f2f9
Ferhat Arslan <ferhat.arslan@student.lab.fi>
2024-04-29 17:08:43 +0300

    ListItems component implemented and AddItems is fixed. Added that app is persistent.

--- App.tsx
+++ App.tsx
@@ -7,7 +7,9 @@
  * @format
  */
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
 import {
   SafeAreaView,
   ScrollView,
@@ -27,28 +29,54 @@ import {
 import Header from './src/Components/Header/Header';
 import Footer from './src/Components/Footer/Footer';
 import AddItem from './src/Components/AddItem/AddItem';
+import ListItem from './src/Components/ListItems/ListItems';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export type ShoppingListItem = {
   item: string;
-  quantity: string;
+  quantity: number;
   id: string;
 }
 
-const testShoppingList: ShoppingListItem[] = [
-  {item: "Bananas", quantity: "2", id: "102"},
-  {item: "Apples", quantity: "5", id: "103"}
-];
+const STORAGE_KEY = '@shopping_list';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-
-  const [shoppingList, setShoppingList] = useState<ShoppingListItem[]>(testShoppingList)
+  const [shoppingList, setShoppingList] = useState<ShoppingListItem[]>([]);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  console.log(`ShoppingList ${JSON.stringify(shoppingList)}`)
+  useEffect(() => {
+    const loadShoppingListFromStorage = async () => {
+      try {
+        const stringifiedShoppingList = await AsyncStorage.getItem(STORAGE_KEY);
+        if (stringifiedShoppingList) {
+          setShoppingList(JSON.parse(stringifiedShoppingList));
+        }
+      } catch (e) {
+        console.error("Failed to load the shopping list", e);
+      }
+    };
+
+    loadShoppingListFromStorage();
+  }, []);
+
+  useEffect(() => {
+    const saveShoppingListToStorage = async () => {
+      try {
+        const stringifiedShoppingList = JSON.stringify(shoppingList);
+        await AsyncStorage.setItem(STORAGE_KEY, stringifiedShoppingList);
+      } catch (e) {
+        console.error("Failed to save the shopping list", e);
+      }
+    };
+
+    if (shoppingList.length > 0) {
+      saveShoppingListToStorage();
+    }
+  }, [shoppingList]);
 
   return (
     <SafeAreaView style={[styles.mainContainer, backgroundStyle]}>
@@ -73,7 +101,12 @@ function App(): React.JSX.Element {
       </View>
 
       <View style={styles.listItemsContainer}>
-        <Text>List items</Text>
+        {/* <Text>List items</Text> */}
+        <ListItem
+          shoppingList={shoppingList}
+          setShoppingList={setShoppingList}
+        >
+        </ListItem>
       </View>
 
       <View style={styles.footerContainer}>
@@ -91,23 +124,23 @@ const styles = StyleSheet.create({
   },
   headerContainer: {
     flex: 1,
-    borderWidth: 1,
-    borderColor: 'red',
+    borderWidth: 0.5,
+    //borderColor: 'red',
   },
   addItemContainer: {
     flex: 5,
-    borderWidth: 1,
-    borderColor: 'red',
+    borderWidth: 0.5,
+    // borderColor: 'red',
   },
   listItemsContainer: {
     flex: 5,
-    borderWidth: 1,
-    borderColor: 'red',
+    borderWidth: 0.5,
+    // borderColor: 'red',
   },
   footerContainer: {
     flex: 1,
-    borderWidth: 1,
-    borderColor: 'red',
+    borderWidth: 0.5,
+    // borderColor: 'red',
   },
   headerComponent: {
     backgroundColor: 'lightyellow'
